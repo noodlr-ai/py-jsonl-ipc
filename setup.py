4029dff1239a41642f3a3315333a1888528a43f8
Josh Weston <10539811+Josh-Weston@users.noreply.github.com>
2025-10-19 17:40:12 -0300

    Update result_data type in make_result_envelope to Any and bump version to 0.0.21

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jsonlipc',
-    version='0.0.20',
+    version='0.0.21',
     packages=find_packages(),
     install_requires=[],
     author='Noodlr.ai',
