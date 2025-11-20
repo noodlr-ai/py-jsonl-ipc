bde232676e6938f2f382480aa4893457d356f238
Josh Weston <10539811+Josh-Weston@users.noreply.github.com>
2025-11-20 19:03:38 -0400

    Bump version to 0.0.24 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jsonlipc',
-    version='0.0.23',
+    version='0.0.24',
     packages=find_packages(),
     install_requires=[],
     author='Noodlr.ai',
