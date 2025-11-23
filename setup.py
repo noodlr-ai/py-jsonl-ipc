a1aba690d30c1ce89006a99721d9a16d7b3a3825
Josh Weston <10539811+Josh-Weston@users.noreply.github.com>
2025-11-23 13:53:17 -0400

    Switch from pyenv to v; Bump version to 0.0.25 in setup.py and pyproject.toml; add uv.lock file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jsonlipc',
-    version='0.0.24',
+    version='0.0.25',
     packages=find_packages(),
     install_requires=[],
     author='Noodlr.ai',
